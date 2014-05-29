0ab1c630b9156534e49621e7a3d8174f28358c4a
Laoujin <woutervs@hotmail.com>
2014-05-29 16:42:37 +0200

    fixed google maps

--- contact.php
+++ contact.php
@@ -3,7 +3,9 @@
 	{
 		echo "<td width=520 valign=top id=loopImageCell>";
 		//echo '<iframe width="520" height="347" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=nl&amp;geocode=&amp;q=Ninoofsesteenweg+22,+Ninove,+Vlaams+Gewest,+Belgi%C3%AB&amp;aq=&amp;sll=50.867715,4.018528&amp;sspn=0.00165,0.004037&amp;ie=UTF8&amp;hq=&amp;hnear=Ninoofsesteenweg+22,+9450+Haaltert,+Oost-Vlaanderen,+Vlaams+Gewest,+Belgi%C3%AB&amp;ll=50.867457,4.017992&amp;spn=0.018797,0.044546&amp;z=14&amp;iwloc=A&amp;output=embed&plaatsnaam=Pennelekker"></iframe>';
-		echo '<iframe width="520" height="347" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=restaurant&amp;hl=nl&amp;ie=UTF8&amp;sll=50.867457,4.017992&amp;sspn=0.052977,0.129175&amp;near=Ninoofsesteenweg+22,+9450+Haaltert,+Belgi%C3%AB&amp;geocode=CU0RSlzRmumCFQ4tCAMdXU89AClJ9DQsrLzDRzFspbbgNDEQKQ&amp;vpsrc=0&amp;num=10&amp;t=m&amp;hq=restaurant&amp;hnear=&amp;ll=50.86747,4.018013&amp;spn=0.013245,0.032294&amp;z=14&amp;iwloc=A&amp;cid=5146632773873597586&amp;output=embed"></iframe>';
+		//echo '<iframe width="520" height="347" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=restaurant&amp;hl=nl&amp;ie=UTF8&amp;sll=50.867457,4.017992&amp;sspn=0.052977,0.129175&amp;near=Ninoofsesteenweg+22,+9450+Haaltert,+Belgi%C3%AB&amp;geocode=CU0RSlzRmumCFQ4tCAMdXU89AClJ9DQsrLzDRzFspbbgNDEQKQ&amp;vpsrc=0&amp;num=10&amp;t=m&amp;hq=restaurant&amp;hnear=&amp;ll=50.86747,4.018013&amp;spn=0.013245,0.032294&amp;z=14&amp;iwloc=A&amp;cid=5146632773873597586&amp;output=embed"></iframe>';
+		//echo '<iframe width="520" height="347" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/place/De+Pennelekker/@50.86747,4.01801,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x6e0a5078c3f8096"></iframe>';
+		echo '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2518.039417730493!2d4.0180099999999985!3d50.86746999999991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6e0a5078c3f8096!2sDe+Pennelekker!5e0!3m2!1sen!2s!4v1401374336058" width="600" height="450" frameborder="0" style="border:0"></iframe>';
 		echo "</td>";
 	}
 
@@ -14,7 +16,7 @@ U wilt een optie nemen voor een bepaalde datum? In onze menukaart neuzen? Vrijbl
 Bel ons even op of stuur ons een mailtje. Wilt u graag langskomen, houd er dan rekening mee dat wij in het weekend druk aan het werk zijn. U maakt dus het best even een afspraak om langs te komen.
 <br><br>
 <div align=center>
-<a target=_blank href="http://maps.google.com/maps?q=restaurant&amp;hl=nl&amp;ie=UTF8&amp;sll=50.867457,4.017992&amp;sspn=0.052977,0.129175&amp;near=Ninoofsesteenweg+22,+9450+Haaltert,+Belgi%C3%AB&amp;geocode=CU0RSlzRmumCFQ4tCAMdXU89AClJ9DQsrLzDRzFspbbgNDEQKQ&amp;vpsrc=0&amp;num=10&amp;t=m&amp;hq=restaurant&amp;hnear=&amp;ll=50.86747,4.018013&amp;spn=0.013245,0.032294&amp;z=14&amp;iwloc=A&amp;cid=5146632773873597586&amp;source=embed">Klik hier om de kaart groter te bekijken.</a>
+<a target=_blank href="https://www.google.com/maps/place/De+Pennelekker/@50.86747,4.01801,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x6e0a5078c3f8096">Klik hier om de kaart groter te bekijken.</a>
 </div>
 <?php
 	include_once 'include/pagefooter.php';
